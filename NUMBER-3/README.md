93efefaaf0d5a038bcae4c1b437328e558c07afc
raihan-muhammad <raihanmuhammad.dev@gmail.com>
2020-10-24 15:49:57 +0700

    update: updated readme.md

--- NUMBER-3/README.md
+++ NUMBER-3/README.md
@@ -2,3 +2,11 @@
 
 ![expected](./expected.png)
 
+## Problem
+
+Bismillah, Saya sudah malukukan analisis pada aplikasi ini. Kemudian saya menemukan beberapa masalah antara lain :
+
+- props scope seharusnya user saja.
+- Kita harus mendaftarkan aplikasi ini di github dev untuk mendapatkan client id dan secret id
+- Setalah proses login dengan github selesai, kita harus mendapatkan akses token untuk mendapatkan data user yang sudah login. Tapi disini saya mendapatkan problem cors, sebenernya saya pengen buat server tersediri untuk mengatasi ini. Tapi lagi lagi waktu menjadi penghalang.
+- Ambil nama user yang sudah login dari response API
--- NUMBER-3/src/GithubLogin.js
+++ NUMBER-3/src/GithubLogin.js
@@ -71,61 +71,57 @@ class GitHubLogin extends Component {
 
   onGetAccessToken = (code) => {
     // const { clientId, clientSecret } = this.props;
-    // const body = {
-    //   client_id: process.env.REACT_APP_CLIENT_ID,
-    //   client_secret:
-    //     process.env.REACT_APP_CLIENT_SECRET ||
-    //     '8d8edd59846fc4ccc16d06effbdd75c02664572e',
-    //   code: code.code,
-    // };
-    // console.log(body);
-    // const options = {
-    //   headers: {
-    //     accept: 'application/json',
-    //   },
-    // };
-    // axios
-    //   .post(`https://github.com/login/oauth/access_token`, body, options)
-    //   .then((access_token) => {
-    //     console.log(access_token);
-    //     this.onGetProfile(access_token);
+    const body = {
+      client_id: process.env.REACT_APP_CLIENT_ID,
+      client_secret:
+        process.env.REACT_APP_CLIENT_SECRET ||
+        '8d8edd59846fc4ccc16d06effbdd75c02664572e',
+      code: code.code,
+    };
+    console.log(body);
+    const options = {
+      headers: {
+        accept: 'application/json',
+      },
+    };
+    axios
+      .post(`https://github.com/login/oauth/access_token`, body, options)
+      .then((access_token) => {
+        console.log(access_token);
+        this.onGetProfile(access_token);
+      })
+      .catch((err) => this.onFailure(err.message));
+    // const data = new FormData();
+    // data.append('client_id', process.env.REACT_APP_CLIENT_ID);
+    // data.append(
+    //   'client_secret',
+    //   process.env.REACT_APP_CLIENT_SECRET
+    // );
+    // data.append('code', code);
+    // // Request to exchange code for an access token
+    // fetch(`https://github.com/login/oauth/access_token`, {
+    //   method: 'POST',
+    //   headers: new Headers().append('Content-Type', 'text/plain'),
+    //   body: data,
+    // })
+    //   .then((response) => response.text())
+    //   .then((paramsString) => {
+    //     let params = new URLSearchParams(paramsString);
+    //     const access_token = params.get('access_token');
+    //     const scope = params.get('scope');
+    //     const token_type = params.get('token_type');
+    //     // Request to return data of a user that has been authenticated
+    //     return fetch(
+    //       `https://api.github.com/user?access_token=${access_token}&scope=${scope}&token_type=${token_type}`
+    //     );
     //   })
-    //   .catch((err) => this.onFailure(err.message));
-
-    const data = new FormData();
-    data.append('client_id', process.env.REACT_APP_CLIENT_ID);
-    data.append(
-      'client_secret',
-      process.env.REACT_APP_CLIENT_SECRET ||
-        '8d8edd59846fc4ccc16d06effbdd75c02664572e'
-    );
-    data.append('code', code);
-
-    // Request to exchange code for an access token
-    fetch(`https://github.com/login/oauth/access_token`, {
-      method: 'POST',
-      headers: new Headers().append('Content-Type', 'text/plain'),
-      body: data,
-    })
-      .then((response) => response.text())
-      .then((paramsString) => {
-        let params = new URLSearchParams(paramsString);
-        const access_token = params.get('access_token');
-        const scope = params.get('scope');
-        const token_type = params.get('token_type');
-
-        // Request to return data of a user that has been authenticated
-        return fetch(
-          `https://api.github.com/user?access_token=${access_token}&scope=${scope}&token_type=${token_type}`
-        );
-      })
-      .then((response) => response.json())
-      .then((response) => {
-        console.log(response);
-      })
-      .catch((error) => {
-        console.log(error);
-      });
+    //   .then((response) => response.json())
+    //   .then((response) => {
+    //     console.log(response);
+    //   })
+    //   .catch((error) => {
+    //     console.log(error);
+    //   });
   };
 
   onGetProfile = (token) => {
